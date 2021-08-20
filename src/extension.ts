ff317b83bddbc580ece016ef03cf73c8a1b9a60b
skarab42 <skarab@bluewin.ch>
2021-08-20 15:02:17 +0200

    activation on command

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,13 @@
-export function activate() {
-  console.log("prout :)");
+import * as vscode from "vscode";
+
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+  subscriptions.push(
+    vscode.commands.registerCommand("twitch-skarab42.twitch-login", () => {
+      console.log("prout :)");
+    })
+  );
+}
+
+export function deactivate() {
+  console.log("deactivated");
 }
